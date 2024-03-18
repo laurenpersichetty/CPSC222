e2521f78323b1e398f9313444a40b2314c7518d3
lauren <lauren@CPSC222.myguest.virtualbox.org>
2024-03-17 20:35:52 -0400

    Final Python Code

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, redirect
 import pwd
 import grp
 
@@ -12,21 +12,29 @@ def authenticate():
     if not auth or auth.username != USERNAME or auth.password != PASSWORD:
             return False
     return True
-@app.route('/api/users', methods=['GET','POST'])
+@app.route('/api/users', methods=['POST'])
 def users():
+    if request.method != 'POST':
+            return jsonify({'error': 'Method Not Allowed'}), 405
     if not authenticate():
             return jsonify({'error':'Unauthorized access1111'}), 401
     users_info = {}
     for user in pwd.getpwall():
             users_info[str(user.pw_uid)] = user.pw_name
     return jsonify(users_info)
-@app.route('/api/groups', methods = ['GET','POST'])
+@app.route('/api/groups', methods = ['POST'])
 def groups():
+if request.method != 'POST':
+            return jsonify({'error': 'Method Not Allowed'}), 405
     if not authenticate():
             return jsonify({'error':'Unauthorized access2222'}),401
     groups_info = {}
     for group in grp.getgrall():
             groups_info[str(group.gr_gid)] = group.gr_name
     return jsonify(groups_info)
+@app.route('/')
+def web():
+    return send_file('web.html')
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=3000)
+        app.run(host='127.0.0.1', port=3000)
